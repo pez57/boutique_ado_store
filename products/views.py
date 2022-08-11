df8de39b52125290a89f3f76427e09aebcf96fd7
pez57 <perry_s@live.com>
2022-08-11 09:49:07 +0000

    Fixed image widget for admin add and update product

--- products/views.py
+++ products/views.py
@@ -73,10 +73,10 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-    """Add product to store"""
+    """ Add a product to the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse, ('home'))
+        return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -89,20 +89,20 @@ def add_product(request):
     else:
         form = ProductForm()
         
-    form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
+
     return render(request, template, context)
 
 
 @login_required
 def edit_product(request, product_id):
-    """Edit a product"""
+    """ Edit a product in the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse, ('home'))
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -112,7 +112,7 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure form is valid.')
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -122,6 +122,7 @@ def edit_product(request, product_id):
         'form': form,
         'product': product,
     }
+
     return render(request, template, context)
 
 
@@ -130,7 +131,7 @@ def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse, ('home'))
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -19,7 +18,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure form is valid.')
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -32,14 +31,14 @@ def profile(request):
     }
 
     return render(request, template, context)
-    
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a past confirmation for order nymber {order_number}.'
-        'A confirmation email was sent on the order date'
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
     ))
 
     template = 'checkout/checkout_success.html'
